0e78b6f0c86253950a51654b97567fb38abaa564
Subba Rao <subba.rao@tietoevry.com>
2026-06-05 16:38:03 +0530

    Updated example codes related to selenium, requests library

---
+++ SeleniumFramework/Pages/LoginPage.py
@@ -0,0 +1,22 @@
+from selenium.webdriver.common.by import By
+
+from Pages.ShopPage import ShopPage
+from Utils.BrowserUtils import BrowserUtils
+
+
+class LoginPage(BrowserUtils):
+    def __init__(self, driver):
+        super().__init__(driver)
+        self.driver = driver
+        self.username_input = (By.ID, "username")
+        self.password = (By.NAME, "password")
+        self.sign_button = (By.ID, "signInBtn")
+
+# * before self in place of locator will place the above tuple value in required format
+    def login(self, username, password):
+        self.driver.find_element(*self.username_input).send_keys(username)
+        self.driver.find_element(*self.password).send_keys(password)
+        self.driver.find_element(*self.sign_button).click()
+        shop_page = ShopPage(self.driver)
+        return shop_page
+
